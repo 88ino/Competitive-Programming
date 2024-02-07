bf28ca9d4941e2a27accfb261b0eed426c7a5baa
tq1d <107417782+88ino@users.noreply.github.com>
2024-02-07 23:49:35 +0900

    Add files via upload

--- Atcoder/AtCoder Problems/ABC-B問題/281-300.py
+++ Atcoder/AtCoder Problems/ABC-B問題/281-300.py
@@ -147,42 +147,178 @@ print("".join(T))
 
 
 # 291
-
+N = int(input())
+X = list(map(int, input().split()))
+X.sort()
+ans = sum(X[N:-N]) / (3*N)
+print(ans)
 
 
 # 292
+from collections import defaultdict, deque
+import imp
+N, Q = map(int, input().split())
+p = defaultdict()
+for i in range(1, N+1):
+    p[i] = 0
 
+for _ in range(Q):
+    c, x = map(int, input().split())
+    if c == 1:
+        p[x] += 1
+    elif c == 2:
+        p[x] += 2
+    else:
+        if p[x] >= 2:
+            print("Yes")
+        else:
+            print("No")
 
 
 # 293
-
+N = int(input())
+A = list(map(int, input().split()))
+B = set()
+for i in range(N):
+    p = i+1
+    if p not in B:
+        B.add(A[i])
+ans = list(set(i for i in range(1, N+1)) - B)
+ans.sort()
+print(len(ans))
+print(*ans)
 
 
 # 294
-
+H, W = map(int, input().split())
+for i in range(H):
+    s = []
+    a = list(map(int, input().split()))
+    for j in range(W):
+        if a[j] == 0:
+            s.append(".")
+        else:
+            s.append(chr(ord("A") + a[j] - 1))
+    print("".join(s))
 
 
 # 295
+def dist(r1, c1, r2, c2):
+    return abs(r1-r2) + abs(c1-c2)
 
+R, C = map(int, input().split())
+field = []
+bomb = []
+for i in range(R):
+    s = list(input())
+    field.append(s)
+    for j in range(C):
+        if s[j]!="#" and s[j]!=".":
+            power = int(s[j])
+            bomb.append((i, j, power))
+
+for i in range(R):
+    for j in range(C):
+        for b in bomb:
+            r1, c1, p = b
+            r2, c2 = i, j
+            d = dist(r1, c1, r2, c2)
+            if d <= p:
+                field[r2][c2] = "."
+
+for f in field:
+    print("".join(f))
 
 
 # 296
-
+h = "abcdefgh"
+w = "12345678"
+for i in range(8):
+    s = input()
+    for j in range(8):
+        if s[j] == "*":
+            print(h[j] + w[7-i])
 
 
 # 297
+S = input()
+a = 0
+for i in range(8):
+    if S[i] == "B":
+        a += i
+if a%2==0:
+    print("No")
+    exit()
 
+for i in range(8):
+    if S[i] == "R":
+        for j in range(i+1, 8):
+            if S[j] == "R":
+                print("No")
+                exit()
+            elif S[j] == "K":
+                print("Yes")
+                exit()
 
 
 # 298
+import numpy as np
 
+N = int(input())
+A = [list(map(int, input().split())) for _ in range(N)]
+B = [list(map(int, input().split())) for _ in range(N)]
+A = np.array(A)
+B = np.array(B)
+for _ in range(4):
+    if np.min(B-A) >= 0:
+        print("Yes")
+        exit()
+    A = np.rot90(A)
+print("No")
 
 
 # 299
-
+N, T = map(int, input().split())
+C = list(map(int, input().split()))
+R = list(map(int, input().split()))
+if T in C:
+    win = T
+else:
+    win = C[0]
+B = []
+for i in range(N):
+    if C[i] == win:
+        B.append((R[i], i+1))
+B.sort(reverse=True)
+print(B[0][1])
 
 
 # 300
+def w_shift(m):
+    a = []
+    for i in range(1, H):
+        a.append(m[i])
+    a.append(m[0])
+    return a
 
+def h_shift(m):
+    a = []
+    for i in range(H):
+        a.append([*m[i][1:], m[i][0]])
+    return a
 
+H, W = map(int, input().split())
+A = [list(input()) for _ in range(H)]
+B = [list(input()) for _ in range(H)]
+for i in range(H):
+    if A == B:
+        print("Yes")
+        exit()
+    for j in range(W):
+        A = h_shift(A)
+        if A == B:
+            print("Yes")
+            exit()
+    A = w_shift(A)
+print("No")
 
